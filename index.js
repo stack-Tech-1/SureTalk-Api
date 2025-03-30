73ae717e3ea01afce88cd58188017e0d19747502
stack-Tech-1 <204887589+stack-Tech-1@users.noreply.github.com>
2025-03-30 18:31:54 +0100

    byscript update

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const { db, FieldValue } = require('./firebase');
-const bcrypt = require('bcryptjs');
+const bcrypt = require('bcrypt');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
 const dns = require('dns').promises;
