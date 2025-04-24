62f98cebb62096432c4b5896b1f0216b4396833b
stack-Tech-1 <stacktechnologies0@gmail.com>
2025-04-24 15:50:37 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1561,26 +1561,24 @@ app.post('/start-payment-setup', async (req, res) => {
 
     console.log('✅ Subscription created for customer:', customer.id);
 
-    // TwiML continues the Studio flow
-    res.set('Content-Type', 'text/xml');
-    res.send(`
-      <Response>
-        <Say>Thank you! Your payment was processed successfully.</Say>
-        <Redirect method="POST">https://webhooks.twilio.com/v1/Accounts/${process.env.TWILIO_ACCOUNT_SID}/Flows/${process.env.STUDIO_FLOW_SID}?FlowEvent=return</Redirect>
-      </Response>
-    `);
+      // TwiML continues the Studio flow
+      res.set('Content-Type', 'text/xml');
+      res.send(`
+        <Response>
+          <Say>Thank you! Your payment was processed successfully.</Say>          
+        </Response>
+      `);
 
-  } catch (err) {
-    console.error('Payment processing error:', err);
-    
-    res.set('Content-Type', 'text/xml');
-    res.send(`
-      <Response>
-        <Say>We encountered an error processing your payment. Please try again later.</Say>
-        <Redirect method="POST">https://webhooks.twilio.com/v1/Accounts/${process.env.TWILIO_ACCOUNT_SID}/Flows/${process.env.STUDIO_FLOW_SID}?FlowEvent=return</Redirect>
-      </Response>
-    `);
-  }
+    } catch (err) {
+      console.error('Payment processing error:', err);
+      
+      res.set('Content-Type', 'text/xml');
+      res.send(`
+        <Response>
+          <Say>We encountered an error processing your payment. Please try again later.</Say>          
+        </Response>
+      `);
+    }
 });
 
 
