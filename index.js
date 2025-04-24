f10f77b4e88658262c7c7759c40eae8b4058cff8
stack-Tech-1 <stacktechnologies0@gmail.com>
2025-04-24 16:48:28 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1566,10 +1566,9 @@ app.post('/start-payment-setup', async (req, res) => {
       res.send(`
         <Response>
           <Say>Thank you! Your payment was processed successfully.</Say>
-          <Hangup />
+          <Redirect method="POST">https://webhooks.twilio.com/v1/Accounts/${process.env.TWILIO_ACCOUNT_SID}/Flows/${process.env.STUDIO_FLOW_SID}?FlowEvent=return&amp;foo=bar</Redirect>
         </Response>
       `);
-
 
     } catch (err) {
       console.error('Payment processing error:', err);
@@ -1577,11 +1576,10 @@ app.post('/start-payment-setup', async (req, res) => {
       res.set('Content-Type', 'text/xml');
       res.send(`
         <Response>
-          <Say>sorry! Your payment was not processed successfully.</Say>
-          <Hangup />
+          <Say>We encountered an error processing your payment. Please try again later.</Say>
+          <Redirect method="POST">https://webhooks.twilio.com/v1/Accounts/${process.env.TWILIO_ACCOUNT_SID}/Flows/${process.env.STUDIO_FLOW_SID}?FlowEvent=return&amp;foo=bar</Redirect>
         </Response>
       `);
-
     }
 });
 
